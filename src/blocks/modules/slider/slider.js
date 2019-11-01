4b8fcb2fd27a1eff72bb8c6f5df1e162981f13da
savayer <sava0094@gmail.com>
2019-11-02 02:18:43 +0800

    made skew bg in header with use before, changed png to svg, and other fixes from customer

--- src/blocks/modules/slider/slider.js
+++ src/blocks/modules/slider/slider.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'slick-carousel';
 
 $('.slider__items').slick({
-    slidesToScroll: 1,
+    slidesToScroll: 4,
     slidesToShow: 4,
     infinite: true,
     autoplay: true,
