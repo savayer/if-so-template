daba979842e2c04cde231c43cd977854b9e32af5
savayer <sava0094@gmail.com>
2019-11-01 18:49:13 +0800

    fixed animation of clouds

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    const $left = document.querySelector('.header__left-cloud')
+    const $rightUpper = document.getElementById('right_upper_cloud')
+    const $right = document.getElementById('right_cloud')
+
+    $left.classList.add('active')
+    $rightUpper.classList.add('active')
+    $right.classList.add('active')
+}
