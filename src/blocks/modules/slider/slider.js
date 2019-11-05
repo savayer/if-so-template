5ff42a1fdfc50c8243c646ea90e9caa0fc27ae84
savayer <sava0094@gmail.com>
2019-11-05 14:14:48 +0800

    mobile view of how_work and slider sections

--- src/blocks/modules/slider/slider.js
+++ src/blocks/modules/slider/slider.js
@@ -8,4 +8,28 @@ $('.slider__items').slick({
     autoplay: true,
     speed: 700,
     autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+        
 })
