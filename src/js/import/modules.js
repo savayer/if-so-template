4de356a2001f095e363320426537845a061f5770
savayer <sava0094@gmail.com>
2019-11-27 14:37:47 +0800

    created features list

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,13 +1,13 @@
 import "%modules%/navigation/navigation";
 import "%modules%/header/header";
-import "%modules%/about/about";
+/* import "%modules%/about/about";
 import "%modules%/reasons/reasons";
 import "%modules%/how_work/how_work";
 import "%modules%/slider/slider";
 import "%modules%/visitors/visitors";
 import "%modules%/conditions/conditions";
 import "%modules%/ready_get/ready_get";
-import "%modules%/footer/footer"
+import "%modules%/footer/footer" */
 import AOS from 'aos';
 
 AOS.init({
