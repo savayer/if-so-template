0c252b8ec8569f9eef79f3d21fcfd14e05d6791f
savayer <sava0094@gmail.com>
2019-11-28 01:07:15 +0800

    changed animation of faq section like accordeon on vanillaJS, added wrapper on plans page

--- src/blocks/modules/Page-plans/faq/faq.js
+++ src/blocks/modules/Page-plans/faq/faq.js
@@ -1,7 +1,23 @@
 const $items = document.querySelectorAll('.faq__item')
 
+const removeOtherSelected = () => {
+    $selected = document.querySelectorAll('.selected')
+    $selected.forEach(el => {
+        el.classList.remove('selected')
+        el.querySelector('.faq__answer').style.height = 0
+    })
+}
+
 $items.forEach(item => {
     item.addEventListener('click', function() {
-        this.querySelector('.faq__answer').classList.toggle('active');
+        let $answer = this.querySelector('.faq__answer')
+        if (this.classList.contains('selected')) {
+            this.classList.remove('selected')            
+            $answer.style.height = 0
+        } else {
+            removeOtherSelected()
+            this.classList.add('selected')
+            $answer.style.height = $answer.scrollHeight + 'px'
+        }
     })
 })
