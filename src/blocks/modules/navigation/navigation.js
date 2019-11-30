9b48c5b84f68a2b054d423b24d87fdfd84c1e138
savayer <sava0094@gmail.com>
2019-11-30 18:47:08 +0800

    small fixes

--- src/blocks/modules/navigation/navigation.js
+++ src/blocks/modules/navigation/navigation.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     const $burger = document.getElementById('toggle_menu')
-    const $menuWrapper = document.querySelector('.navigation__menu_wrapper');
+    const $menuWrapper = document.querySelector('.navigation__menu_wrapper')
     const $body = document.querySelector('body')
     const $wrapper = document.querySelector('.wrapper')
 
     $burger.addEventListener('click', function() {
         this.classList.toggle('active');
-        $menuWrapper.classList.toggle('active');
+        $menuWrapper.classList.toggle('active');        
         $body.classList.toggle('overflow-hidden');
         $wrapper.classList.toggle('overflow-hidden');
     })    
