a65f213d89eb9e61e282d4fca2ac5da80dd21d79
savayer <sava0094@gmail.com>
2019-12-05 23:41:36 +0800

    (upd) updated visitors section

--- src/blocks/modules/visitors/visitors.js
+++ src/blocks/modules/visitors/visitors.js
@@ -1,24 +1,27 @@
 const $title = document.querySelectorAll('.visitors__title span')
 const $subTitle1 = $title[0]
-const $subTitle2 = $title[1]
+//const $subTitle2 = $title[1]
 
 const $introtext = document.querySelectorAll('.visitors__introtext span')
 const $intro1 = $introtext[0]
-const $intro2 = $introtext[1]
+//const $intro2 = $introtext[1]
+
+const $thirdText = document.querySelectorAll('.visitors__third_text span');
+const $third1 = $thirdText[0]
+const $third2 = $thirdText[1]
 
 const $bubble1 = document.querySelector('.visitors__bubble1')
 const $bubble2 = document.querySelector('.visitors__bubble2')
 
 const $image = document.querySelector('.visitors__illustration')
 
-if ($title && $introtext && $bubble1 && $bubble2 && $image) {
+if ($title && $introtext && $bubble1 && $thirdText && $bubble2 && $image) {
     if (document.documentElement.clientWidth <= 992) {    
         $subTitle1.setAttribute('data-aos-delay', 0)
-        $subTitle2.setAttribute('data-aos-delay', 50)
+        //$subTitle2.setAttribute('data-aos-delay', 50)
 
         $intro1.setAttribute('data-aos-delay', 250)
-        $intro2.setAttribute('data-aos-delay', 350)
-        
+        //$intro2.setAttribute('data-aos-delay', 350)        
         
         $image.setAttribute('data-aos-delay', 600)
         
