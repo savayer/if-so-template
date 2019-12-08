804a0bc200143513b9c635e414f2b29594d19f5d
savayer <sava0094@gmail.com>
2019-12-08 23:01:34 +0800

    fixed animation of appearing, deleted blur div

--- src/blocks/modules/Free-trial-modal/modal.js
+++ src/blocks/modules/Free-trial-modal/modal.js
@@ -1,19 +1,45 @@
 const $links = document.querySelectorAll('a[data-modal="modal"]')
 const $modal = document.querySelector('.modal')
+const $modalOverlay = document.querySelector('.modal__overlay')
+const $modalForm = document.querySelector('.modal__form')
 const $closeModal = document.querySelector('.modal__close')
+
+import $ from 'jquery'
+
+function animateOverlay(el, dur, close = false, closeOverlay = false) {
+    let fake = {}
+
+    fake.fake = close ? 0 : 100
+    $(el).animate(fake, {
+        step(now, fx) {
+            now = 100 - now
+            $(this).css('transform', 'translateX(' + now + '%)');
+        },
+        done() {
+            if (closeOverlay) {
+                $modal.classList.remove('showed')
+            }
+        },
+        duration: dur
+    }, 'ease-out')
+}
 
 if ($links && $modal) {
     $links.forEach(link => {
         link.addEventListener('click', e => {
             e.preventDefault()
             document.body.classList.add('overflow-hidden');
-            $modal.classList.add('show-modal')
-        })    
+            $modal.classList.add('showed')
+
+            animateOverlay($modalOverlay, 670)
+            animateOverlay($modalForm, 1200)
+        })
     })
 }
 if ($closeModal) {
     $closeModal.addEventListener('click', () => {
-        $modal.classList.remove('show-modal')
         document.body.classList.remove('overflow-hidden');
+        animateOverlay($modalOverlay, 500, true, true)
+        animateOverlay($modalForm, 400, true)
     })
 }
