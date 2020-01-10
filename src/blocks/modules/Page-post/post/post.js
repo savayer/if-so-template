ce1b88b08da4f0a98f5a34817527165a9210512a
savayer <sava0094@gmail.com>
2020-01-10 12:39:58 +0800

    created related slider articles

--- src/blocks/modules/Page-post/post/post.js
+++ src/blocks/modules/Page-post/post/post.js
@@ -2,5 +2,21 @@ import $ from 'jquery';
 
 $('.slider_articles').slick({
     slidesToScroll: 1,
-    slidesToShow: 3
+    slidesToShow: 3,
+    nextArrow: '<i class="slider-next material-icons">chevron_right</i>',
+    prevArrow: '<i class="slider-prev material-icons">chevron_left</i>',
+    responsive: [
+        {
+            breakpoint: 701,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 451,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 })
