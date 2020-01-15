28ead101e205f5520e15af6bf7a8d281f8087a8f
savayer <sava0094@gmail.com>
2020-01-15 22:15:09 +0800

    fixes animation, explode bubble when it comes to the upper part of the block, changed header sizes - wip

--- src/blocks/modules/Page-affiliates/aff-header/header.js
+++ src/blocks/modules/Page-affiliates/aff-header/header.js
@@ -1,29 +1,34 @@
 const text = document.querySelector('.aff_bg__text')
+let left = 0;
+
+if (document.documentElement.clientWidth <= 1600) {
+    left = -4;
+}
 
 switch (text.innerText.length) {
     case 3:
-        text.style.left = '8%'
+        text.style.left = left+8+'%'
         break;
     case 4:
-        text.style.left = '6%'
+        text.style.left = left+6+'%'
         break;
     case 5:
-        text.style.left = '6%'
+        text.style.left = left+6+'%'
         break;
     case 6:
-        text.style.left = '5%'
+        text.style.left = left+5+'%'
         break;
     case 7:
-        text.style.left = '4%'
+        text.style.left = left+4+'%'
         break;
     case 8:
-        text.style.left = '3%'
+        text.style.left = left+3+'%'
         break;
     case 9:
-        text.style.left = '2%'
+        text.style.left = left+2+'%'
         break;
     case 10:
-        text.style.left = '2%'
+        text.style.left = left+2+'%'
         break;
     default:
         break;
--- src/blocks/modules/Page-affiliates/program/program.js
+++ src/blocks/modules/Page-affiliates/program/program.js
@@ -1,16 +1,18 @@
 const $bubbleLg = document.querySelector('.bubble100--lg')
 const $bubbleSm = document.querySelector('.bubble100--sm')
 
-$bubbleLg.addEventListener('animationend', function() {
-    this.classList.add('disableAnimation')
-    
-    $bubbleSm.classList.remove('disableAnimation')
-    $bubbleSm.classList.add('activeSmAnimation')
+$bubbleLg.addEventListener('animationend', function(e) {
+    if (e.animationName === 'explode') {
+        this.classList.add('disableAnimation')
+        $bubbleSm.classList.remove('disableAnimation')
+        $bubbleSm.classList.add('activeSmAnimation')
+    }
 })
 
-$bubbleSm.addEventListener('animationend', function() {
-    this.classList.add('disableAnimation')  
-
-    $bubbleLg.classList.remove('disableAnimation')
-    $bubbleLg.classList.add('activeLgAnimation')
+$bubbleSm.addEventListener('animationend', function(e) {
+    if (e.animationName === 'explode') {
+        this.classList.add('disableAnimation')  
+        $bubbleLg.classList.remove('disableAnimation')
+        $bubbleLg.classList.add('activeLgAnimation')
+    }
 })
