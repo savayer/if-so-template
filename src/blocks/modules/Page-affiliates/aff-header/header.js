775b93a533af1ad97ae6d6988ffee4df8e5e3596
savayer <sava0094@gmail.com>
2020-01-16 22:21:11 +0800

    fixes positions images on shirts, add resize event to fix the positions

--- src/blocks/modules/Page-affiliates/aff-header/header.js
+++ src/blocks/modules/Page-affiliates/aff-header/header.js
@@ -1,45 +1,56 @@
-const text = document.querySelector('.aff_bg__text')
-let left = 0;
-const w = document.documentElement.clientWidth
 
-if (w <= 1600) {
-    left = -4;
-}
-
-if (w <= 1050) {
-    left = -2;
-}
-
-if (w <= 500) {
-    left = -4;
-}
-if (text) {
-    switch (text.innerText.length) {
-        case 3:
-            text.style.left = left+8+'%'
-            break;
-        case 4:
-            text.style.left = left+6+'%'
-            break;
-        case 5:
-            text.style.left = left+6+'%'
-            break;
-        case 6:
-            text.style.left = left+5+'%'
-            break;
-        case 7:
-            text.style.left = left+4+'%'
-            break;
-        case 8:
-            text.style.left = left+3+'%'
-            break;
-        case 9:
-            text.style.left = left+2+'%'
-            break;
-        case 10:
-            text.style.left = left+2+'%'
-            break;
-        default:
-            break;
+const setLogoPostion = () => {
+    const text = document.querySelector('.aff_bg__text')
+    const w = document.documentElement.clientWidth
+    let left = 0;
+    
+    if (w <= 1600) {
+        left = -4;
+    }
+    
+    if (w <= 1050) {
+        left = -2;
+    }
+    
+    if (w <= 500) {
+        left = -4;
+    }
+    if (text) {
+        switch (text.innerText.length) {
+            case 3:
+                text.style.left = left+8+'%'
+                break;
+            case 4:
+                text.style.left = left+6+'%'
+                break;
+            case 5:
+                text.style.left = left+6+'%'
+                break;
+            case 6:
+                text.style.left = left+5+'%'
+                break;
+            case 7:
+                text.style.left = left+4+'%'
+                break;
+            case 8:
+                text.style.left = left+3+'%'
+                break;
+            case 9:
+                text.style.left = left+2+'%'
+                break;
+            case 10:
+                text.style.left = left+2+'%'
+                break;
+            default:
+                break;
+        }
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    setLogoPostion()
+})
+
+window.addEventListener('resize', () => {
+    setLogoPostion()
+})
