85e18942f19097470bbf3cd8ae00bc1250261165
savayer <sava0094@gmail.com>
2020-01-16 20:59:31 +0800

    fixes js, mobile view for program block

--- src/blocks/modules/Page-affiliates/aff-header/header.js
+++ src/blocks/modules/Page-affiliates/aff-header/header.js
@@ -13,32 +13,33 @@ if (w <= 1050) {
 if (w <= 500) {
     left = -4;
 }
-
-switch (text.innerText.length) {
-    case 3:
-        text.style.left = left+8+'%'
-        break;
-    case 4:
-        text.style.left = left+6+'%'
-        break;
-    case 5:
-        text.style.left = left+6+'%'
-        break;
-    case 6:
-        text.style.left = left+5+'%'
-        break;
-    case 7:
-        text.style.left = left+4+'%'
-        break;
-    case 8:
-        text.style.left = left+3+'%'
-        break;
-    case 9:
-        text.style.left = left+2+'%'
-        break;
-    case 10:
-        text.style.left = left+2+'%'
-        break;
-    default:
-        break;
+if (text) {
+    switch (text.innerText.length) {
+        case 3:
+            text.style.left = left+8+'%'
+            break;
+        case 4:
+            text.style.left = left+6+'%'
+            break;
+        case 5:
+            text.style.left = left+6+'%'
+            break;
+        case 6:
+            text.style.left = left+5+'%'
+            break;
+        case 7:
+            text.style.left = left+4+'%'
+            break;
+        case 8:
+            text.style.left = left+3+'%'
+            break;
+        case 9:
+            text.style.left = left+2+'%'
+            break;
+        case 10:
+            text.style.left = left+2+'%'
+            break;
+        default:
+            break;
+    }
 }
--- src/blocks/modules/Page-affiliates/program/program.js
+++ src/blocks/modules/Page-affiliates/program/program.js
@@ -1,18 +1,22 @@
 const $bubbleLg = document.querySelector('.bubble100--lg')
 const $bubbleSm = document.querySelector('.bubble100--sm')
 
-$bubbleLg.addEventListener('animationend', function(e) {
-    if (e.animationName === 'explode') {
-        this.classList.add('disableAnimation')
-        $bubbleSm.classList.remove('disableAnimation')
-        $bubbleSm.classList.add('activeSmAnimation')
-    }
-})
+if ($bubbleLg) {
+    $bubbleLg.addEventListener('animationend', function(e) {
+        if (e.animationName === 'explode') {
+            this.classList.add('disableAnimation')
+            $bubbleSm.classList.remove('disableAnimation')
+            $bubbleSm.classList.add('activeSmAnimation')
+        }
+    })
+}
 
-$bubbleSm.addEventListener('animationend', function(e) {
-    if (e.animationName === 'explode') {
-        this.classList.add('disableAnimation')  
-        $bubbleLg.classList.remove('disableAnimation')
-        $bubbleLg.classList.add('activeLgAnimation')
-    }
-})
+if ($bubbleSm) {
+    $bubbleSm.addEventListener('animationend', function(e) {
+        if (e.animationName === 'explode') {
+            this.classList.add('disableAnimation')  
+            $bubbleLg.classList.remove('disableAnimation')
+            $bubbleLg.classList.add('activeLgAnimation')
+        }
+    })
+}
