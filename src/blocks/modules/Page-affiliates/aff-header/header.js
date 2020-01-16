08133882f961737d7c729ca8bb345102a0ecd0bc
savayer <sava0094@gmail.com>
2020-01-17 01:44:40 +0800

    refactoring, mobile view for join section

--- src/blocks/modules/Page-affiliates/aff-header/header.js
+++ src/blocks/modules/Page-affiliates/aff-header/header.js
@@ -1,49 +1,50 @@
 
 const setLogoPostion = () => {
     const text = document.querySelector('.aff_bg__text')
-    const w = document.documentElement.clientWidth
-    let left = 0;
-
-    if (w <= 1600) {
-        left = -4;
-    }
     
-    if (w <= 1050) {
-        left = -2;
-    }
-    
-    if (w <= 500) {
-        left = -4;
-    }
     if (text) {
+        const w = document.documentElement.clientWidth
+        let left = 0;
+
+        if (w <= 1600) {
+            left = -4;
+        }
+        
+        if (w <= 1050) {
+            left = -2;
+        }
+        
+        if (w <= 500) {
+            left = -4;
+        }
+        
+        let $l
         switch (text.innerText.length) {
             case 3:
-                text.style.left = left+8+'%'
+                $l = left+8+'%'
                 break;
             case 4:
-                text.style.left = left+6+'%'
-                break;
             case 5:
-                text.style.left = left+6+'%'
+                $l = left+6+'%'
                 break;
             case 6:
-                text.style.left = left+5+'%'
+                $l = left+5+'%'
                 break;
             case 7:
-                text.style.left = left+4+'%'
+                $l = left+4+'%'
                 break;
             case 8:
-                text.style.left = left+3+'%'
+                $l = left+3+'%'
                 break;
             case 9:
-                text.style.left = left+2+'%'
-                break;
             case 10:
-                text.style.left = left+2+'%'
+                $l = left+2+'%'
                 break;
             default:
                 break;
         }
+
+        text.style.left = $l
     }
 }
 
--- src/blocks/modules/Page-affiliates/program/program.js
+++ src/blocks/modules/Page-affiliates/program/program.js
@@ -20,3 +20,57 @@ if ($bubbleSm) {
         }
     })
 }
+
+const setTextPosition = () => {
+    const text = document.querySelector('.man_on_wheel__text')    
+
+    if (text) {
+        const w = document.documentElement.clientWidth
+        let right = 0, $r
+
+        if (w <= 780) {
+            right = 15
+        }
+        if (w <= 530) {
+            right = 5
+        }
+        if (w <= 450) {
+            right = 15
+        }
+
+        switch (text.innerText.length) {
+            case 7:
+            case 8:
+                $r = -15 - right + '%'
+                break
+            case 9:
+            case 10:
+                $r = -18 - right + '%'
+                break
+            case 11:
+            case 12:
+                $r = -23 - right + '%'
+                break
+            case 13:
+            case 14:
+                $r = -26 - right + '%'
+                break
+            case 15:
+            case 16:
+                $r = -29 - right + '%'
+                break
+            default:
+                $r = '-13%'
+                break;
+        }
+        text.style.right = $r
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTextPosition()
+})
+
+window.addEventListener('resize', () => {
+    setTextPosition()
+})
