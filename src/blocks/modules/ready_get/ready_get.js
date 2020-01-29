e9d7c9142b5da724bc1683db9432bb6059b987de
savayer <sava0094@gmail.com>
2020-01-29 10:51:23 +0800

    added page 404, magician man with stars adding in ready get section

--- src/blocks/modules/ready_get/ready_get.js
+++ src/blocks/modules/ready_get/ready_get.js
@@ -11,19 +11,50 @@ const $bubble2 = document.querySelector('.ready_get__bubble2')
 const $image = document.querySelector('.ready_get__illustration')
 
 if ($title && $introtext && $button && $bubble1 && $bubble2 && $image) {
-    if (document.documentElement.clientWidth <= 992) {    
+    if (document.documentElement.clientWidth <= 992) {
         $subTitle1.setAttribute('data-aos-delay', 0)
         $subTitle2.setAttribute('data-aos-delay', 50)
 
-        $introtext.setAttribute('data-aos-delay', 250)    
+        $introtext.setAttribute('data-aos-delay', 250)
 
-        $button.setAttribute('data-aos-delay', 350)    
-        
+        $button.setAttribute('data-aos-delay', 350)
+
         $image.setAttribute('data-aos-delay', 600)
-        
-        
+
+
         $bubble1.setAttribute('data-aos-delay', 1000)
         $bubble2.setAttribute('data-aos-delay', 1100)
-        
+
     }
+}
+
+const man = document.querySelector('.ready_get__illustration')
+if (man) {
+    const manMagic = document.querySelector('.ready_get__illustration--magic')
+    const title = document.querySelector('.ready_get__title')
+    const stars = man.querySelector('.stars')
+    let animated = false
+
+    const callback = function (mutationsList) {
+        mutationsList.forEach((mutation) => {
+            if ( mutation.target.classList.contains('aos-animate') && !animated) {
+                animated = true
+                setTimeout(() => {
+                    stars.classList.add('active')
+                    /* man.classList.add('unactive')
+                    manMagic.classList.add('active') */
+                }, 4500)
+                setTimeout(() => {
+                    title.querySelector('span').innerHTML = "Let's make"
+                    title.querySelector('span:nth-child(2)').innerHTML = "magic!"
+                }, 6000)
+            }
+        });
+    };
+    const observer = new MutationObserver(callback)
+
+    observer.observe(man, {
+        attributes: true,
+        attributeFilter: ['class']
+    })
 }
