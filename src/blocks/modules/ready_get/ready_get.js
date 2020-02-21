3978f498936fae44b8976f13e02e34ff8c615951
savayer <sava0094@gmail.com>
2020-02-21 16:29:40 +0800

    use image in svg, show offer to make magic, start and magician man

--- src/blocks/modules/ready_get/ready_get.js
+++ src/blocks/modules/ready_get/ready_get.js
@@ -30,9 +30,8 @@ if ($title && $introtext && $button && $bubble1 && $bubble2 && $image) {
 
 const man = document.querySelector('.ready_get__illustration')
 if (man) {
-    const manMagic = document.querySelector('.ready_get__illustration--magic')
+    const svg = man.querySelector('.ready_get__svg')
     const title = document.querySelector('.ready_get__title')
-    const stars = man.querySelector('.stars')
     let animated = false
 
     const callback = function (mutationsList) {
@@ -40,14 +39,23 @@ if (man) {
             if ( mutation.target.classList.contains('aos-animate') && !animated) {
                 animated = true
                 setTimeout(() => {
-                    stars.classList.add('active')
-                    /* man.classList.add('unactive')
-                    manMagic.classList.add('active') */
-                }, 4500)
+                    svg.querySelector('.stars').classList.add('active')
+                }, 3000)
                 setTimeout(() => {
-                    title.querySelector('span').innerHTML = "Let's make"
-                    title.querySelector('span:nth-child(2)').innerHTML = "magic!"
-                }, 6000)
+                    svg.querySelector('.usual-man').classList.add('unactive')
+                    svg.querySelector('.magician').classList.add('active')
+                }, 3800)
+                setTimeout(() => {
+                    title.classList.add('hide')
+                    title.addEventListener('transitionend', function(e) {
+                        if (e.propertyName === 'opacity') {
+                            title.querySelector('span').innerHTML = "Let's make"
+                            title.querySelector('span:nth-child(2)').innerHTML = "magic!"
+                            title.classList.add('magic')
+                            title.classList.remove('hide')
+                        }
+                    })
+                }, 5000)
             }
         });
     };
