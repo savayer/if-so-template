eabe295fcb655d69a050b5d4cdf7964d5f50d54d
savayer <sava0094@gmail.com>
2020-02-22 16:33:17 +0800

    added animation to pointers

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,6 +3,12 @@ window.onload = function() {
     const $rightUpper = document.getElementById('right_upper_cloud')
     const $right = document.getElementById('right_cloud')
 
+    const headerNew = document.querySelector('.header__perspective')
+
+    if (headerNew) {
+        headerNew.classList.add('loaded')
+    }
+
     if ($left && $rightUpper && $right) {
         $left.classList.add('active')
         $rightUpper.classList.add('active')
