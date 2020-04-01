a568aaa0a8cf8d458f79fa483a7270015ec59adc
savayer <sava0094@gmail.com>
2020-04-01 18:42:10 +0800

    prepared google hero page chunks

--- src/blocks/modules/navigation/navigation.js
+++ src/blocks/modules/navigation/navigation.js
@@ -4,19 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const $menuWrapper = document.querySelector('.navigation__menu_wrapper')
     const $body = document.querySelector('body')
     const $wrapper = document.querySelector('.wrapper')
+    if ($burger) {
+        $burger.addEventListener('click', function() {
+            this.classList.toggle('active');
+            $menuWrapper.classList.toggle('active');        
+            $body.classList.toggle('overflow-hidden');
+            $wrapper.classList.toggle('overflow-hidden');
+        })
+    }
 
-    $burger.addEventListener('click', function() {
-        this.classList.toggle('active');
-        $menuWrapper.classList.toggle('active');        
-        $body.classList.toggle('overflow-hidden');
-        $wrapper.classList.toggle('overflow-hidden');
-    })
-
-    const parent = document.querySelectorAll('.menu-item-has-children')[1]
-    const link = parent.children[0]
-
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        link.nextElementSibling.classList.toggle('active')
-    })
+    if (document.querySelectorAll('.menu-item-has-children')) {
+        const parent = document.querySelectorAll('.menu-item-has-children')[1]
+        if (!parent) return;
+        const link = parent.children[0]
+        
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            link.nextElementSibling.classList.toggle('active')
+        })
+    }
 })
