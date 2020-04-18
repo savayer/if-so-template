02db625e1f997ce39b6c26f5b5ae96184da510fe
savayer <sava0094@gmail.com>
2020-04-18 22:30:09 +0800

    (add) replace modal toggle icon to the flag after first close modal

--- src/blocks/modules/user-modal/user-modal.js
+++ src/blocks/modules/user-modal/user-modal.js
@@ -5,10 +5,23 @@ window.addEventListener('load', e => {
     const closeModalButton = document.querySelector('.if-so-user-modal__close')
     const closeButtonHideTheHand = document.querySelector('.open_if-so-user-modal__close')
     const neverShowButton = document.querySelector('.open_if-so-user-modal__never-show')
+    
+    const openModalHebrewBlock = document.querySelector('.open_if-so-user-modal--hebrew')
+    const closeButtonHideHebrew = document.querySelector('.open_if-so-user-modal__button_close.hebrew-close')
+    const openModalButtonHebrew = document.querySelector('.open_if-so-user-modal__il-icon')
 
     if (handOpenModal && localStorage.getItem('show_modal') !== 'false') {
         handOpenModal.classList.add('active')
     }    
+
+    const openModal = (hebrew = true) => {
+        modal.classList.add('opened')
+        handOpenModal.classList.remove('active')
+        document.body.classList.add('overflow-hidden')
+        if (!hebrew) {
+            modal.classList.add('opened-using-hand')
+        }
+    }
 
     closeButtonHideTheHand.addEventListener('click', () => {
         handOpenModal.classList.remove('active')
@@ -20,13 +33,23 @@ window.addEventListener('load', e => {
     })
 
     openModalButton.addEventListener('click', () => {
-        modal.classList.add('opened')
-        handOpenModal.classList.remove('active')
-        document.body.classList.add('overflow-hidden')
+        openModal(false);
     })
 
-    closeModalButton.addEventListener('click', () => {
+    closeModalButton.addEventListener('click', () => {        
         modal.classList.remove('opened')
         document.body.classList.remove('overflow-hidden')
+        if (modal.classList.contains('opened-using-hand')) {
+            openModalHebrewBlock.classList.add('active')
+            modal.classList.remove('opened-using-hand')
+        }
+    })
+
+    openModalButtonHebrew.addEventListener('click', () => {
+        openModal();
+    })
+    
+    closeButtonHideHebrew.addEventListener('click', () => {
+        openModalHebrewBlock.classList.remove('active')
     })
 })
