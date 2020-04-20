f644377bd46e4f6b928d011482efa2d973ebdc4d
savayer <sava0094@gmail.com>
2020-04-20 20:45:21 +0800

    (add) expand accordingly menu in sidebar in docs page

--- src/blocks/modules/Page-docs/docs/docs.js
+++ src/blocks/modules/Page-docs/docs/docs.js
@@ -22,3 +22,25 @@ widgetTitles.forEach(widgetTitle => {
         $content.style.height = $content.scrollHeight + 'px'
     })
 })
+
+
+/**
+ * Expand accordingly widget menu block for wp
+ */
+
+const findPageId = () => {
+    const bodyClassList = document.body.classList
+    for (let cclass of bodyClassList) {
+        if (cclass.includes('page-id-')) {
+            return '.sidebar .page-item-' + cclass.split('-')[2]
+        }
+    }
+}
+
+const pageItem = document.querySelector(findPageId())
+if (pageItem) {
+    const $parent = pageItem.closest('.widget')
+    const $content = $parent.querySelector('.widget__content')
+    $parent.classList.add('selected')
+    $content.style.height = $content.scrollHeight + 'px'    
+} 
